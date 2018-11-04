0fd52ee38a1ed93ed1987558bc513f31a2a6a748
assassin945 <1311620084@qq.com>
2018-11-04 19:46:44 +0800

    2018/11/4 - 2

--- Server/ServerControl.cs
+++ Server/ServerControl.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Server
@@ -23,6 +24,20 @@ namespace Server
             serverSocket.Bind(new IPEndPoint(IPAddress.Any, 12345));
             serverSocket.Listen(10);
             Console.WriteLine("服务器启动成功");
+
+            Thread threadAccept = new Thread(Accept);
+            threadAccept.IsBackground = true;
+            threadAccept.Start();
+        }
+
+        private void Accept()
+        {
+            //接受客户端的方法，会挂起当前线程
+            Socket client = serverSocket.Accept();
+            IPEndPoint point = client.RemoteEndPoint as IPEndPoint;
+            Console.WriteLine(point.Address + "[" + point.Port + "]连接成功");
+
+            Accept();
         }
     }
 }
